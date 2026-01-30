beeadc2b4f61892f016baa2a0cbf23d0122d5e7c
Mousom <mousomray02@gmail.com>
2026-01-30 12:26:25 +0530

    Bbasasas

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express()
 
 app.use(cors(
     {
-        origin : ["http://localhost:3000"],
+        origin : [process.env.FRONTEND_URL],
         methods: ["GET", "POST", "PUT", "PUTCH", "DELETE"],
         credentials: true
     }
